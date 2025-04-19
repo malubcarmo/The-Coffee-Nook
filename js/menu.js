b4780e2da59a8372524d2ff72acdc94b60da6841
Heiki1 <heiki1chan@gmail.com>
2025-04-19 20:15:52 -0300

    Add forms to contact and new folder for scripts

---
+++ js/menu.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const navLinks = document.querySelectorAll('.navbar-nav .nav-link');
+  const navbarToggler = document.querySelector('.navbar-toggler');
+  const navbarCollapse = document.getElementById('navbarNav');
+
+  navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+      // Fecha o menu (modo mobile)
+      if (window.innerWidth < 768 && navbarCollapse.classList.contains('show')) {
+        new bootstrap.Collapse(navbarCollapse).hide();
+      }
+    });
+  });
+});
